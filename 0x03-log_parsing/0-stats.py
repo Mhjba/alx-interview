c7bee7dd48b214429aadfba8db32877afd2e0a17
Mhjba <mahjoubaaitalla15@gmail.com>
2024-07-17 10:47:23 -0700

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,51 @@
 #!/usr/bin/python3
-"""Log Parser"""
+"""
+Task 0. Log parsing
+
+A script that reads stdin line by line and computes metrics.
+"""
+
 import sys
 
 
-if __name__ == '__main__':
-    file_size = [0]
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
+def printStats(file_size, status):
+    """printStats
 
-    def print_stats():
-        """ Print statistics """
-        print('File size: {}'.format(file_size[0]))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print('{}: {}'.format(key, status_codes[key]))
+    This function takes the total file size and the
+    statues that were called and prints them.
 
-    def parse_line(line):
-        """ Checks the line for matches """
-        try:
-            line = line[:-1]
-            word = line.split(' ')
-            # File size is last parameter on stdout
-            file_size[0] += int(word[-1])
-            # Status code comes before file size
-            status_code = int(word[-2])
-            # Move through dictionary of status codes
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
-            pass
+    Arguments:
+        file_size (int): The total file size to be printed.
+        status (dict{int, int}): A dictionary of the statues that were called.
+    """
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
 
-    linenum = 1
-    try:
-        for line in sys.stdin:
-            parse_line(line)
-            """ print after every 10 lines """
-            if linenum % 10 == 0:
-                print_stats()
-            linenum += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+
+total_file_size = 0
+count = 0
+possible_status = {200: 0, 301: 0, 400: 0, 401: 0,
+                   403: 0, 404: 0, 405: 0, 500: 0}
+try:
+    for line in sys.stdin:
+        args = line.split()
+
+        status_code = int(args[-2])
+        file_size = int(args[-1])
+
+        if status_code in possible_status:
+            possible_status[status_code] += 1
+
+        total_file_size += file_size
+        count += 1
+
+        if count == 10:
+            printStats(total_file_size, possible_status)
+            count = 0
+    printStats(total_file_size, possible_status)
+except KeyboardInterrupt:
+    raise
+finally:
+    printStats(total_file_size, possible_status)
